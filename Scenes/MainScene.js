78140212a744ec801d5c40db5047b74c71c8f25a
Piero Morais <piero.morais@gmail.com>
2024-01-26 23:35:46 -0300

    add moviment for two players

--- Scenes/MainScene.js
+++ Scenes/MainScene.js
@@ -9,17 +9,50 @@ export default class MainScene extends Phaser.Scene {
     preload() {
         this.load.image('bed', '../assets/bed-example.jpg');
         this.load.image('pillow', '../assets/pillow.png');
+        this.load.image('pillow2', '../assets/pillow2.png');
     }
 
     create() {
         this.add.image(512, 200, 'bed');
         this.pillow = this.physics.add.image(512,400, 'pillow').setScale(0.2).refreshBody();
+        this.pillow2 = this.physics.add.image(200,400, 'pillow2').setScale(0.2).refreshBody();
     
         this.pillow.setCollideWorldBounds(true);
+        this.pillow2.setCollideWorldBounds(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        
+        this.A_key = this.input.keyboard.addKey('A');
+        this.D_key = this.input.keyboard.addKey('D');
+        this.W_key = this.input.keyboard.addKey('W');
     }
 
     update() {
-        
+        // player 1
+        if (this.A_key.isDown) {
+            this.pillow.setVelocityX(-180);
+        }else if (this.D_key.isDown){
+            this.pillow.setVelocityX(180);
+        }
+        else {
+            this.pillow.setVelocityX(0);
+        }
+        if (this.W_key.isDown){ //  && this.pillow.body.touching.down
+            this.pillow.setVelocityY(-500);
+        }
+
+        // player 2
+        if (this.cursors.left.isDown) {
+            this.pillow2.setVelocityX(-180);
+        }else if (this.cursors.right.isDown){
+            this.pillow2.setVelocityX(180);
+        }
+        else {
+            this.pillow2.setVelocityX(0);
+        }
+        if (this.cursors.up.isDown){ //  && this.pillow.body.touching.down
+            this.pillow2.setVelocityY(-500);
+        }
     }
 
 }
