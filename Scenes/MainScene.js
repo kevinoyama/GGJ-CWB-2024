da3fcf9e59cb9f709d3b47d4a37a9a329ebc89bd
kevinoyama <kevinoyama01@gmail.com>
2024-01-26 23:10:31 -0300

    First Pillow Character - MVP

--- Scenes/MainScene.js
+++ Scenes/MainScene.js
@@ -8,10 +8,14 @@ export default class MainScene extends Phaser.Scene {
 
     preload() {
         this.load.image('bed', '../assets/bed-example.jpg');
+        this.load.image('pillow', '../assets/pillow.png');
     }
 
     create() {
         this.add.image(512, 200, 'bed');
+        this.pillow = this.physics.add.image(512,400, 'pillow').setScale(0.2).refreshBody();
+    
+        this.pillow.setCollideWorldBounds(true);
     }
 
     update() {
