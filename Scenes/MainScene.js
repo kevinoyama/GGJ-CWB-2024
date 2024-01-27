920babaff3cea09cded17a29eeedba72e81b4011
kevinoyama <kevinoyama01@gmail.com>
2024-01-27 14:35:00 -0300

    Merge changes from Davi with New Sprites for pillow

--- Scenes/MainScene.js
+++ Scenes/MainScene.js
@@ -12,19 +12,22 @@ export default class MainScene extends Phaser.Scene {
     preload() {
         this.load.image('bed', '../assets/bed-example.jpg');
         this.load.image('pillowSpecialAttack', '../assets/sprite_31.png');
-        this.load.spritesheet('pillow', '../assets/pillow-sprite.png', {
-            frameWidth: 28, frameHeight: 27
+        this.load.spritesheet('pillow', '../assets/pillow.png', {
+            frameWidth: 300, frameHeight: 400
         });
+        this.load.spritesheet('capy', '../assets/capy_sprite.png', {
+            frameWidth: 32, frameHeight: 32
+        });
+
     }
 
     create() {
-
         this.add.image(512, 200, 'bed');
-        this.pillow = this.physics.add.sprite(200,200,'pillow').setScale(5).refreshBody();
-        this.pillow2 = this.physics.add.sprite(200,200,'pillow').setScale(5).refreshBody();
+        this.pillow = this.physics.add.sprite(200, 200, 'pillow').setScale(0.75).refreshBody(); //.setScale(7).refreshBody();
+        this.capy = this.physics.add.sprite(560, 200, 'capy', 4).setScale(10).refreshBody();
 
         var spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        spaceKey.on("down", function(event) {
+        spaceKey.on("down", function (event) {
             this.launchPillowSpecialAttack();
         }, this);
 
@@ -43,37 +46,67 @@ export default class MainScene extends Phaser.Scene {
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('pillow', { start: 0, end: 4}),
-            frameRate: 10,
+            frames: this.anims.generateFrameNumbers('pillow', { start: 0, end: 3 }),
+            frameRate: 20,
             repeat: -1
         });
 
         this.anims.create({
             key: 'turn',
-            frames: [{key: 'pillow', frame: 4}],
+            frames: [{ key: 'pillow', frame: 0 }],
             frameRate: 20
         });
 
         this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNumbers('pillow', {
+                start: 8, end: 15
+            }),
+            frameRate: 0.01,
+            repeat: 1,
+        });
+
+        this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('pillow', { start: 5, end: -8}),
-            frameRate: 10,
+            frames: this.anims.generateFrameNumbers('pillow', { start: 0, end: 3 }),
+            frameRate: 20,
             repeat: -1
         });
 
         this.anims.create({
             key: 'punch-right',
-            frames: this.anims.generateFrameNumbers('pillow', { start: 9, end: 9}),
+            frames: this.anims.generateFrameNumbers('pillow', { start: 20, end: 23 }),
             frameRate: 30,
             repeat: -1,
-            duration: 1000 
+            duration: 1000
+        });
+
+        //animação para a capivara
+
+        this.anims.create({
+            key: 'capy-right',
+            frames: this.anims.generateFrameNumbers('capy', { start: 6, end: 9 }),
+            frameRate: 20,
+            repeat: -1
         });
         
+        this.anims.create({
+            key: 'capy-left',
+            frames: this.anims.generateFrameNumbers('capy', { start: 6, end: 9 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'capy-turn',
+            frames: [{key: 'capy', frame: 4}],
+            frameRate: 20,
+            repeat: -1
+        });
+
         this.pillow.setCollideWorldBounds(true);
-        this.pillow2.setCollideWorldBounds(true);
-        this.physics.add.collider(this.pillow, this.pillow2);
-        
-        
+        this.capy.setCollideWorldBounds(true);
+        this.physics.add.collider(this.pillow, this.capy);
 
         this.cursors = this.input.keyboard.createCursorKeys();
         this.A_key = this.input.keyboard.addKey('A');
@@ -122,36 +155,6 @@ checkWorldBounds() {
         this.pillow.x = this.game.config.width;
         this.pillow.setVelocityX(0);
     }
-        // this.pillowSpecialAttack.rotation += 0.1;
- 
-         // // player 1
-         // if (this.A_key.isDown) {
-         //     if (this.counter >= 40) {
-         //         this.counter = 0;
-         //     }else {this.counter += 1} 
- 
-         //     this.pillow.setVelocityX(-180);
-         //     this.pillow.setRotation(-this.counter*3.15/20);
-         
-         // }else if (this.D_key.isDown){
-             
-         //     if (this.counter >= 40) {
-         //         this.counter = 0;
-         //     }else {this.counter += 1} 
-         //     this.pillow.setVelocityX(180);
-         //     this.pillow.setRotation(this.counter*3.15/20);
- 
-         // }
-         // else {
-         //     this.pillow.setVelocityX(0);
-         //     this.pillow.setRotation(0);
- 
-         // }
-         // if (this.W_key.isDown){ //  && this.pillow.body.touching.down
-         //     this.pillow.setVelocityY(-500);
-         // }
- 
-         // player 2
          if (this.cursors.left.isDown) {
             /*  if (this.counter2 >= 40) {
                  this.counter2 = 0;
@@ -198,6 +201,5 @@ checkWorldBounds() {
      }
  
  }
- 
  
 
