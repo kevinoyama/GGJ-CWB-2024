27012847a106e292e60c396fae36d28816e76432
Piero Morais <piero.morais@gmail.com>
2024-01-28 11:59:34 -0300

    player class

---
+++ Players/Player.js
@@ -0,0 +1,165 @@
+export default class Player {
+    constructor(game, config, playerId) {
+        this.game = game;
+        this.config = config;
+        this.playerId = playerId;
+        this.totalOfAttacks = 5;
+        this.isDefending = false;
+        this.lifeHealth = 100;
+        this.attack_audio_key = this.config.name + 'attack_audio';
+        // anims keys
+        this.walk_key = this.config.name + '-walk';
+        this.steady_key = this.config.name + '-steady';
+        this.defend_key = this.config.name + '-defend';
+        this.jump_key = this.config.name + '-jump';
+        this.fall_key = this.config.name + '-fall';
+        this.attack_key = this.config.name + '-attack';
+        this.special_attack_key = this.config.name + '-special-attack';
+
+    }
+
+    
+    preload() {
+        this.game.load.spritesheet(this.config.name, this.config.spritesheet, {
+            frameWidth: 300, frameHeight: 400 // 
+        });
+        this.game.load.audio(this.attack_audio_key,[this.config.attack_audio]);
+    }
+
+    create() {
+        this.player = this.game.physics.add.sprite(this.config.start_x, this.config.start_y, this.config.name).setScale(0.75).refreshBody();
+        this.specialAttacks = this.game.physics.add.group();
+        this.player.setCollideWorldBounds(true);
+        this.createAnims();
+        this.commands = {
+            right: this.playerId == 1 ? this.game.input.keyboard.addKey('D') : this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
+            left: this.playerId == 1 ? this.game.input.keyboard.addKey('A') : this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT),
+            jump: this.playerId == 1 ? this.game.input.keyboard.addKey('W') : this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP),
+            defend: this.playerId == 1 ? this.game.input.keyboard.addKey('S') : this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN),
+            special_attack: this.playerId == 1 ? this.game.input.keyboard.addKey('F') : this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT),
+            attack: this.playerId == 1 ? this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE) : this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CTRL)
+        }
+
+        this.attackSound = this.game.sound.add(this.attack_audio_key);
+
+        //var spaceKey = this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.commands.special_attack.on("down", function(event) {
+            if (this.totalOfAttacks == 0) {
+                this.game.time.addEvent({
+                    delay: 3000,
+                    callback: this.recharge,
+                    callbackScope: this
+                });
+            }
+
+            if (this.totalOfAttacks > 0) {
+                this.launchSpecialAttack();
+                this.attackSound.play();
+                this.totalOfAttacks -= 1;
+            }
+        }, this);
+        //var downKey = this.game.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+       this.commands.defend.on('down', (event) => {
+            this.isDefending = true;
+            this.player.anims.play(this.defend_key, true);
+        }, this)
+
+        this.commands.defend.on('up', () => {
+            this.isDefending = false;
+        }, this);
+    }
+    update() {
+        this.handleMovements();
+        // this.checkWorldBounds();
+    }
+
+    recharge() {
+        this.totalOfAttacks = 5; 
+    }
+
+    createAnims() {
+        this.game.anims.create({
+            key: this.walk_key,
+            frames: this.game.anims.generateFrameNumbers(this.config.name, { start: 0, end: 5 }),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.game.anims.create({
+            key: this.steady_key,
+            frames: this.game.anims.generateFrameNumbers(this.config.name, {
+                start: 16, end: 19
+            }),
+            frameRate: 10
+        });
+
+        this.game.anims.create({
+            key: this.jump_key,
+            frames: this.game.anims.generateFrameNumbers(this.config.name, {
+                start: 8, end: 11
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.game.anims.create({
+            key: this.fall_key,
+            frames: this.game.anims.generateFrameNumbers(this.config.name, {
+                start: 12, end: 15
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.game.anims.create({
+            key: this.attack_key,
+            frames: this.game.anims.generateFrameNumbers(this.config.name, { start: 20, end: 23 }),
+            frameRate: 20,
+            repeat: 1,
+        });
+
+         this.game.anims.create({
+            key: this.defend_key,
+            frames: this.game.anims.generateFrameNumbers(this.config.name, { start: 20, end: 23 }),
+            frameRate: 30,
+            repeat: 0
+        });
+
+    }
+
+/*     attacking() {
+        this.player.play('pizza-punch', true);
+    } */
+
+    handleMovements() {
+
+        if (this.commands.right.isDown) {
+            this.player.setVelocityX(550);
+            this.player.scaleX = 0.75;
+            this.player.play(this.walk_key, true);
+        } else if (this.commands.left.isDown) {
+            this.player.setVelocityX(-550);
+            this.player.scaleX = -0.75;
+            this.player.anims.play(this.walk_key, true);
+        } 
+        else {
+            this.player.setVelocityX(0);
+            if(!this.isDefending){
+                this.player.play(this.steady_key, true);
+            }
+        }
+
+        if (this.commands.jump.isDown && this.player.body.blocked.down) {
+            this.isDefending = false;
+            this.player.setVelocityY(-1400);
+        } else if (this.player.body.velocity.y < 0) {
+            this.player.play(this.jump_key, true);
+        }
+
+        if(this.player.body.velocity.y > 0){
+            this.player.play(this.fall_key, true);
+        }
+    }
+
+}
+
--- Scenes/MainScene.js
+++ Scenes/MainScene.js
@@ -1,46 +1,60 @@
 import Phaser from 'phaser';
-import Capy from '../Players/Capy';
-import Pillow from '../Players/Pillow';
-import Pizza from '../Players/Pizza';
+
+import Player from '../Players/Player';
 
 export default class MainScene extends Phaser.Scene {
-
+    
     constructor() {
         super();
         this.counter = 0;
         this.counter2 = 0;
         this.life = 3;
-        this.Capy = new Capy(this);
-        this.Pizza = new Pizza(this);
-        //this.Pillow = new Pillow(this);
+
+        this.player1 = new Player(this, {
+            name: 'capy',
+            spritesheet: '../assets/capysprite.png',
+            attack_audio: '../assets/Golpe aplicado capivara.m4a',
+            start_x: 200,
+            start_y: 400
+        }, 1);
+
+        this.player2 = new Player(this, {
+            name: 'pizza',
+            spritesheet: '../assets/pizzasprite.png',
+            attack_audio: '../assets/Golpe aplicado capivara.m4a',
+            start_x: 600,
+            start_y: 400
+        }, 2);
+
+
     }
 
     preload() {
         this.load.image('bed', '../assets/Capybaras_3.jpg');
         this.load.image('pillowSpecialAttack', '../assets/sprite_31.png');
         this.load.audio('audioFarm', ['../assets/Farm effect mp3.mp3']);
-        //this.Pillow.preload();
-        this.Capy.preload();
-        this.Pizza.preload();
+
+        this.player1.preload();
+        this.player2.preload();
     }
 
     create() {
         this.add.image(512, 200, 'bed');
-        //this.Pillow.create();
-        this.Capy.create();
-        this.Pizza.create();
-        this.physics.add.collider(this.Pizza.player, this.Capy.player);
+        // this.physics.add.collider(this.Pizza.player, this.Capy.player);
         this.musicFarm = this.sound.add('audioFarm',{
             loop: true
         });
         this.musicFarm.play();
         this.sound.pauseOnBlur = true;
+        
+        this.player1.create();
+        this.player2.create();
     }
 
     update() {
-        this.Capy.update();
-        //this.Pillow.update();
-        this.Pizza.update();
+
+        this.player1.update();
+        this.player2.update();
     }
 
 }
